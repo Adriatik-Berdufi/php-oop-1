23c086d91154e861a2e7e61654558779da0b34f3
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-04 16:26:43 +0100

    add class productions, and 2 instanze

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+    class Production{
+
+        //prprieta
+        public $title;
+        public $language;
+        public $vote;
+
+        public function __construct($title, $language, $vote){
+            $this->title = $title;
+            $this->language = $language;
+            $this->vote = $vote;
+        }
+
+        public function render()
+        {
+            return " titolo:{$this -> title}  lingua:{$this -> language} voto: {$this -> vote}";
+        }
+        
+    }
+
+$instanza1 = new Production('sfdasfa', 'en', '7.5');
+$instanza2= new Production('sfdasfaasx', 'it', '5.5');
+
+echo $instanza1 -> render();
+echo $instanza2 -> render();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +34,9 @@
     <title>PHP_OOP</title>
 </head>
 <body>
-    
+
+
+
     
 </body>
 </html>
