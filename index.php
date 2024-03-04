cc9cab95929b07893591b0afd0d228161c195497
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-04 17:46:50 +0100

    creato un sempilce template

--- index.php
+++ index.php
@@ -28,7 +28,10 @@ $instanza2= new Production('sfdasfaasx', 'it', '5.5');
 echo $instanza2 -> render(); */
 
 foreach ($productions as $production) {
-    echo $production->render();
+    /* echo "<h2>{$production->title}</h2>";
+    echo "<h2>{$production->language}</h2>";
+    echo "<h2>{$production->vote}</h2>"; */
+
 }
 
 ?>
@@ -38,11 +41,23 @@ foreach ($productions as $production) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP_OOP</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
+    <div class="container d-flex">
+        <h1>Lista Film</h1>
+        <div class="section-film">
+            <?php foreach ($productions as $production) : ?>
+                <div class="card">
+                    <h3>Tittolo: <?= $production->title ?></h3>
+                    <h4>Lingua: <?= $production->language ?></h4>
+                    <h4>Voto: <?= $production->vote ?></h4>
 
+                </div>
+            <?php endforeach; ?>
+        </div>
+       
+    </div>
 
-
-    
 </body>
 </html>
